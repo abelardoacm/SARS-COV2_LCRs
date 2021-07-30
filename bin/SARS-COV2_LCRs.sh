b79a6da0db62b18bca4e4816960029eec1c8c034
abelardoacm <abelardoacm@ciencias.unam.mx>
2021-07-30 09:52:44 -0500

    SARS-COV2_LCRs.sh + user input

--- bin/SARS-COV2_LCRs.sh
+++ bin/SARS-COV2_LCRs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # sample use ./SARS-COV2_LCRs.sh
 #
-#
-window=12
-locut=1.9
-hicut=2.1
+echo "seg parameters"
+echo
+read -p "type window length and hit enter: " window
+echo
+read -p "type locut and hit enter: " locut
+echo
+read -p "type hicut length and hit enter: " hicut
 # INVOKE PIPELINE
 ls -1 ../data/Raw_database/ | sed 's/.gb//g' | while read variante; do ./BD_seg.sh $variante $window $locut $hicut ; done
 ls -1 ../data/Raw_database/ | sed 's/.gb//g' | while read variante; do  Rscript Analyze_LCRs.R $variante ; done
